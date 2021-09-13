07aa2ad899560b0e65f80a3492193ad1518b9c30
ben <kakengi_bernard@hotmail.com>
2021-09-13 15:06:17 +0300

    untracked node_modules

--- server.js
+++ server.js
@@ -1,46 +1,51 @@
-require('express-async-errors')
+require('express-async-errors');
 require('dotenv').config();
 const winston = require('winston');
 const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const app = express();
 
-//console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 //console.log(`app: ${app.get('env')}`)
 
-app.get("/", (req, res) => {
-  res
-      .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
-      .send("<html><head></head><body></body></html>");
-})
+app.get('/', (req, res) => {
+	res
+		.set(
+			'Content-Security-Policy',
+			"default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
+		)
+		.send('<html><head></head><body></body></html>');
+});
 
-require('./startup/logging')
+require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
-require('./startup/config')()
-require('./startup/validation')()
+require('./startup/config')();
+require('./startup/validation')();
 require('./startup/prod')(app);
 
 console.log('Application Name: ' + config.get('name'));
 
 if (app.get('env') === 'development') {
-  app.use(morgan('tiny'));
-  debug('Morgan enabled...');
+	app.use(morgan('tiny'));
+	debug('Morgan enabled...');
 }
 
 if (process.env.NODE_ENV === 'production') {
-  //db = process.env.presenters_db;
-  app.use(express.static(path.join(__dirname, '/client/build')));
+	//db = process.env.presenters_db;
+	app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res, next) =>
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
-  );
+	app.get('*', (req, res, next) =>
+		res.sendFile(path.join(__dirname, 'client/build/index.html'))
+	);
 }
 //PORT
 const PORT = process.env.PORT || config.get('port');
-const server = app.listen(PORT, () => winston.info(`Listening on Port ${PORT}...`));
+const server = app.listen(PORT, () =>
+	winston.info(`Listening on Port ${PORT}...`)
+);
 
-module.exports = server
+module.exports = server;

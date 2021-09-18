db1f83767f9854b564dec8e7bad5e262314ca4df
ben <kakengi_bernard@hotmail.com>
2021-09-18 19:56:30 +0300

    prepare to deploy a server to heroku independently

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const winston = require('winston');
 const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
+const cors = require('cors');
 const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(cors());
+
 // console.log(`NODE_ENV is: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`)
+console.log(`app: ${app.get('env')}`);
 
 require('./startup/logging');
 require('./startup/routes')(app);
@@ -27,24 +30,22 @@ app.get('/', (req, res) => {
 		.send('<html><head></head><body></body></html>');
 });
 
-console.log('Application Name: ' + config.get('name'));
-
 if (app.get('env') === 'development') {
 	app.use(morgan('tiny'));
 	debug('Morgan enabled...');
 }
 
-if (process.env.NODE_ENV === 'production') {
-	//db = process.env.presenters_db;
-	app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+// 	//db = process.env.presenters_db;
+// 	app.use(express.static(path.join(__dirname, 'client/build')));
 
-	console.log(__dirname);
-	console.log(path.join(__dirname, 'client/build'));
+// 	console.log(__dirname);
+// 	console.log(path.join(__dirname, 'client/build'));
 
-	app.get('*', (req, res, next) =>
-		res.sendFile(path.join(__dirname, 'client/build/index.html'))
-	);
-}
+// 	app.get('*', (req, res, next) =>
+// 		res.sendFile(path.join(__dirname, 'client/build/index.html'))
+// 	);
+// }
 //PORT
 const PORT = process.env.PORT || config.get('port');
 const server = app.listen(PORT, () =>
--- startup/config.js
+++ startup/config.js
@@ -1,8 +1,10 @@
 const config = require('config');
 
-
 module.exports = function () {
-  if (!config.get('jwtPrivateKey')) {
-    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
-  }
+	if (!config.get('jwtPrivateKey')) {
+		throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+	}
 };
+
+console.log('Application Name:' + config.get('name'));
+console.log('Mail Server:' + config.get('mail.host'));
